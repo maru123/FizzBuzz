a1b4cd381945aa98b7b619b79b8563ba86285e7f
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2015-07-13 08:53:34 +0900

    1つ目ケース文

--- FizzBuzz1.js
+++ FizzBuzz1.js
@@ -1,7 +1,14 @@
 (function(){
-  for (var i = 1; i < 100; i++
+  for ( var i = 1; i <= 100; i++
   ){
-    print(i);
+    if( i % 15 === 0 ){
+      print("FizzBuzz");
+    } else if( i % 3 === 0 ){
+      print("Fizz");
+    } else if( i % 5 === 0 ) {
+      print("Buzz");
+    } else {
+      print(i);
+    };
   }
-  print("hello,wordl!");
 }());
